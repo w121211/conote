a48c6c0ab2d636fb2c3da35164768dc78522e0d3
Ching Chen <piniyu699@gmail.com>
2021-12-07 22:29:53 +0800

    拿掉user provider

--- src/web/pages/_app.tsx
+++ src/web/pages/_app.tsx
@@ -8,12 +8,12 @@ import ModalProvider from '../components/modal/modalContext'
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const apolloClient = useApollo(pageProps.initialApolloState)
   return (
-    <UserProvider>
-      <ApolloProvider client={apolloClient}>
-        <ModalProvider>
-          <Component {...pageProps} />
-        </ModalProvider>
-      </ApolloProvider>
-    </UserProvider>
+    // <UserProvider>
+    <ApolloProvider client={apolloClient}>
+      <ModalProvider>
+        <Component {...pageProps} />
+      </ModalProvider>
+    </ApolloProvider>
+    // </UserProvider>
   )
 }
