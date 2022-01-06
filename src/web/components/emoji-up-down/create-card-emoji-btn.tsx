b3723b7bb9aef63cd2045751d53816ac7ddb69ad
Ching Chen <piniyu699@gmail.com>
2022-01-07 01:32:39 +0800

    add create emoji btn style

--- src/web/components/emoji-up-down/create-card-emoji-btn.tsx
+++ src/web/components/emoji-up-down/create-card-emoji-btn.tsx
@@ -12,16 +12,18 @@ const CreateCardEmojiBtn = ({ cardId, emojiCode }: { cardId: string; emojiCode: 
     createCardEmoji({ variables: { cardId, code: emojiCode } })
   }
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex items-center gap-1">
       <button
-        className={`btn-reset-style`}
+        className={`btn-reset-style group w-8 h-8 rounded-full ${
+          emojiCode === 'PIN' ? 'hover:bg-red-100' : 'hover:bg-blue-100'
+        }`}
         onClick={() => {
           handleLike()
         }}
       >
-        <EmojiIcon code={emojiCode} />
+        <EmojiIcon className={emojiCode === 'PIN' ? 'text-red-400' : 'text-gray-400'} code={emojiCode} />
       </button>
-      <span className="text-gray-500">{0}</span>
+      <span className="min-w-[10px] text-gray-500 text-sm font-['Red Hat Mono']">{0}</span>
     </div>
   )
 }
